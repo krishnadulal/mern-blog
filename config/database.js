1eff781316a10bc8b37ed2128444c8e22fe4546a
krishnadulal <krishnadulaldalapati@gmail.com>
2021-08-20 00:24:39 +0530

    first commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+const { DB_USERNAME,DB_PASSWORD,DB_CLUSTER } = require("./config");
+
+module.exports = {
+  MongoURI: `mongodb+srv://blog:blog123@cluster0.4clik.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+};
